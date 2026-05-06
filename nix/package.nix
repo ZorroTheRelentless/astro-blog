f4b0b68f63f8748b41c2795f651f1b7c9c75f164
Zoravar Singh Kalkat <zorro@zoravar.eu>
2026-05-07 00:28:19 +0300

    Remove extraneous lib dir in nix build

--- nix/package.nix
+++ nix/package.nix
@@ -1,11 +1,20 @@
 { pkgs }:
-pkgs.buildNpmPackage {
+let
+  docs-package = pkgs.buildNpmPackage {
+    name = "blog-package";
+    src = ../.;
+    npmDepsHash = "sha256-7Vplo6JFKQR5iTLzRsAm5LC3TCiQOcScj3nJIEikJlg=";
+    buildPhase = ''
+      npm run build
+      mkdir -p $out
+      cp -r dist/* $out
+    '';
+};
+in pkgs.stdenv.mkDerivation {
   name = "blog";
   src = ../.;
-  npmDepsHash = "sha256-7Vplo6JFKQR5iTLzRsAm5LC3TCiQOcScj3nJIEikJlg=";
   buildPhase = ''
-    npm run build
     mkdir -p $out
-    cp -r dist/* $out
+    find ${docs-package}/ -mindepth 1 -maxdepth 1 ! -name 'lib' -exec cp -r {} $out/ \;
   '';
 }
