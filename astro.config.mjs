ae44469bcfb4b0ba2b91ba2ebca8860bf02d9afc
Zoravar Singh Kalkat <zorro@zoravar.eu>
2026-06-29 18:59:59 +0300

    Remove sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import { visit } from "unist-util-visit";
-import sitemap from "@astrojs/sitemap";
+// import sitemap from "@astrojs/sitemap";
 
 function externalLinks() {
   return (tree) => {
@@ -25,7 +25,7 @@ function externalLinks() {
 export default defineConfig({
   site: "https://zoravar.eu",
   site: "https://yourdomain.com",
-  integrations: [sitemap()],
+  // integrations: [sitemap()],
   integrations: [mdx()],
   i18n: {
     locales: ["en", "de"],
